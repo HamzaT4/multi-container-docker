92b618c47f15a301449f40dabc59f44951841b78
HamzaT4 <hamzatay2012@gmail.com>
2024-02-19 23:49:10 +0300

    yo

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p></p>
+          
             <h1>Fibonacci Number</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
